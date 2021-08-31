52462ece76b41c2b0416c3ee5bf963f2dfa8409a
Jenniffer Crystine Souza dos Santos <jenniffer.souza@iteris.com.br>
2021-08-31 19:14:34 -0300

    adiciona artigo

---
+++ src/app/components/artigo-cpnt/artigo/artigo.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ArtigoModel } from 'src/app/services/artigo/artigo-model';
+
+@Component({
+  selector: 'app-artigo',
+  templateUrl: './artigo.component.html',
+  styleUrls: ['./artigo.component.css']
+})
+export class ArtigoComponent implements OnInit {
+  @Input() artigo: ArtigoModel | undefined;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
